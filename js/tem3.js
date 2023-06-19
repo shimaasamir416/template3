728cae04b52609689c1069003a812044c2f53714
shimaasamir416 <130293883+shimaasamir416@users.noreply.github.com>
2023-06-19 17:21:26 +0300

    Add files via upload

---
+++ js/tem3.js
@@ -0,0 +1,8 @@
+var x=document.querySelector("#spanhead")
+var y=document.querySelector(".special")
+var z=document.querySelector("#drop")
+x.onclick=function(){
+    y.style.backgroundColor="white"
+    y.style.padding="20px"
+    z.style.marginBottom="20px"
+}
